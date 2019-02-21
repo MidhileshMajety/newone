d9b19e75b0480af43fd2a21c95ac685ca3b71daa
MidhileshMajety <midhileshmajeyti@gmail.com>
2019-02-21 12:38:56 +0530

    my midhilesh

---
+++ m-1/src/main/java/Midh/m_1/App.java
@@ -0,0 +1,13 @@
+package Midh.m_1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
